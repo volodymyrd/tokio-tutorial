4287fc7dfb3ec93cab302acb8ea2b33de4e9b1fc
volodymyrd <volodymyrdotsenko@gmail.com>
2024-11-20 21:10:15 +0100

    add mini server.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# tokio-tutorial
+# [tokio-tutorial](https://tokio.rs/tokio/tutorial)
+
 Tokio Tutorial codelab
+
+```
+cargo install mini-redis
+```
+
+```
+mini-redis-server
+```
+
+```
+lsof -i :6379
+kill -9 <PID>
+```
--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,17 +1,59 @@
-use mini_redis::{client, Result};
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    // Open a connection to the mini-redis address.
-    let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    // Set the key "hello" with value "world"
-    client.set("hello", "word".into()).await?;
+    loop {
+        // The second item contains the IP and port of the new connection.
+        let (socket, _) = listener.accept().await.unwrap();
 
-    // Get key "hello"
-    let result = client.get("hello").await?;
+        // process(socket).await;
 
-    println!("got value from the server; result={:?}", result);
+        // A new task is spawned for each inbound socket. The socket is
+        // moved to the new task and processed there.
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
 
-    Ok(())
+    async fn process(socket: TcpStream) {
+        use mini_redis::Command::{self, Get, Set};
+        use std::collections::HashMap;
+
+        // A hashmap is used to store data
+        let mut db = HashMap::new();
+
+        // Connection, provided by `mini-redis`, handles parsing frames from
+        // the socket
+        let mut connection = Connection::new(socket);
+
+
+        // Use `read_frame` to receive a command from the connection.
+        while let Some(frame) = connection.read_frame().await.unwrap() {
+            let response = match Command::from_frame(frame).unwrap() {
+                Set(cmd) => {
+                    // The value is stored as `Vec<u8>`
+                    db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                    Frame::Simple("OK".to_string())
+                }
+                Get(cmd) => {
+                    if let Some(value) = db.get(cmd.key()) {
+                        // `Frame::Bulk` expects data to be of type `Bytes`. This
+                        // type will be covered later in the tutorial. For now,
+                        // `&Vec<u8>` is converted to `Bytes` using `into()`.
+                        Frame::Bulk(value.clone().into())
+                    } else {
+                        Frame::Null
+                    }
+                }
+                cmd => panic!("unimplemented {:?}", cmd),
+            };
+
+            // Write the response to the client
+            connection.write_frame(&response).await.unwrap();
+        }
+    }
 }
